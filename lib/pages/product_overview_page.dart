6ebac850009b916b8909bd00928358e15902c76c
Luana Dias <luana.dias@colab.re>
2023-12-05 19:11:06 -0300

    feat: criando componente grid

--- lib/pages/product_overview_page.dart
+++ lib/pages/product_overview_page.dart
@@ -1,11 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'package:shop/models/product.model.dart';
-
-import '../components/product_item.dart';
-import '../models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProductOverviewPage extends StatelessWidget {
   const ProductOverviewPage({
@@ -14,24 +9,13 @@ class ProductOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10),
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(10),
+        child: ProductGrid(),
       ),
     );
   }
