f2e32dc0547905459905cbbfdace1e1ab5b161d1
Luana Dias <luana.dias@colab.re>
2023-12-07 20:23:24 -0300

    feat: filtrando favoritos parte 1

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,7 +5,26 @@ import 'package:shop/models/product.model.dart';
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
-  List<Product> get items => [..._items];
+  //List<Product> get items => [..._items];
+
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return _items;
+  }
+
+  bool _showFavoriteOnly = false;
+
+  void showOnlyFavorite() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners(); //serve como um setState ou um observer
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/product_overview_page.dart
+++ lib/pages/product_overview_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import '../components/product_grid.dart';
 
 class ProductOverviewPage extends StatelessWidget {
@@ -9,9 +11,34 @@ class ProductOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider =
+        Provider.of<ProductList>(context); //instancia do ProductList
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert_sharp),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favoritos,
+                child: Text('favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all, //apenas o enum do popup
+                child: Text('todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedvalue) {
+              if (selectedvalue == FilterOptions.favoritos) {
+                provider
+                    .showOnlyFavorite(); //funcao do product_list q retorna favorite caso true
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: const Padding(
         padding: EdgeInsets.all(10),
@@ -20,3 +47,5 @@ class ProductOverviewPage extends StatelessWidget {
     );
   }
 }
+
+enum FilterOptions { favoritos, all }
