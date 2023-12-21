b431e6bb97630aee4e44da9482f7755796b1f271
Luana Dias <luana.dias@colab.re>
2023-12-21 18:16:14 -0300

    feat: adicionar pedidos

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/cart_item.dart';
+import 'package:shop/models/order_list.dart';
 
 import '../models/cart.dart';
 
@@ -39,7 +40,13 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(onPressed: () {}, child: const Text('FINALIZAR'))
+                  TextButton(
+                      onPressed: () {
+                        Provider.of<OrderList>(context, listen: false)
+                            .addOrder(cart);
+                        cart.clear();
+                      },
+                      child: const Text('FINALIZAR'))
                 ],
               ),
             ),
