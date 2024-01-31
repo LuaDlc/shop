50575f07358759d429cc76d1f84e3a696c488432
Luana Dias <luana.dias@colab.re>
2024-01-30 21:59:27 -0300

    feat: confirmar exclusao com showdialog

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.model.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -29,7 +31,35 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog<bool>(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Tem certeza?'),
+                          content: const Text('exlcuir produto'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(ctx).pop(true);
+                              },
+                              child: const Text('sim'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(false);
+                              },
+                              child: const Text('nao'),
+                            )
+                          ],
+                        )).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).removeProduct(product);
+                  }
+                });
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).disabledColor,
             ),
