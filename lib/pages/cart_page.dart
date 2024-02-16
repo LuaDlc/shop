4e8aa722ad80c75d557692c56140947ed9f92892
Luana Dias <luana.dias@colab.re>
2024-02-16 20:10:52 -0300

    fix: armazenando os pedidos no firebase

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -46,13 +46,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(
-                      onPressed: () {
-                        Provider.of<OrderList>(context, listen: false)
-                            .addOrder(cart);
-                        cart.clear();
-                      },
-                      child: const Text('FINALIZAR'))
+                  CartButton(cart: cart)
                 ],
               ),
             ),
@@ -65,5 +59,40 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  //extrai o botao de comprar
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    await Provider.of<OrderList>(context, listen: false)
+                        .addOrder(widget.cart);
+                    widget.cart.clear();
+                    setState(() => _isLoading = false);
+                  },
+            child: const Text('FINALIZAR'));
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-lua-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-lua-default-rtdb.firebaseio.com/orders';
 }
