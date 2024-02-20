a7cf6cfce767eb628a2702af12972f1b1556eb0c
Luana Dias <luana.dias@colab.re>
2024-02-20 17:12:17 -0300

    edit readme

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ A new Flutter project.
 
 ## Getting Started
 
-Novo app de loja usando Provider
+ Uma  loja que  Provider como DB
 
-Em desenvolvimento...
+- Modelo de produto criado
+- Componente Grid de produtos, que é o componente inicial que irá buildar todos os produtos do modelo de produto
+- Componente que lista os produtos dentro do grid de produtos
+- Tela de detalhe dos produtos, uma tela simples que traz detalhes e usa o ModalRoute como argumento os detalhes dos produtos
+- Aprendi um pouco sobre o que é estado.
+- 
 
 
--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -7,17 +7,44 @@ import 'package:shop/models/order_list.dart';
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
+  // Future<void> _refreshOrders(BuildContext context) {
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Meus Pedidos'),
-        ),
-        drawer: const AppDrawer(),
-        body: ListView.builder(
-          itemCount: orders.itemsCount,
-          itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-        ));
+      appBar: AppBar(
+        title: const Text('Meus Pedidos'),
+      ),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            //verifica se o estado esta esperando ou nao
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text('Ocorreu um erro'),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
+
+/*usando o future builder, ajuda a trabalhar com codigo assincrono
+e ter acesso ao estado da conexao e saber entao se esta no modo waiting
+saber se gerou algum errro e tratar diferentes cenarios
+trabalha com requisicoes assincronas usando future
+
+obs: podemos trabalhar com um widget do tipo statelesss e ter o estado
+*/
