619fc366620f20e4950c25091dc69fc2029efbb3
Luana Dias <luana.dias@colab.re>
2024-03-18 19:59:46 -0300

    fix: usando mais animacoes

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -25,7 +25,8 @@ class _AuthFormState extends State<AuthForm>
   };
 
   AnimationController? _controller;
-  Animation<Size>? _heightAnimation;
+  Animation<double>? _opacityAnimation;
+  Animation<Offset>? _slideAnimation;
 
   @override
   void initState() {
@@ -39,15 +40,19 @@ class _AuthFormState extends State<AuthForm>
       ),
     ); //vscin ;e a propria classe implementa e tem os metodos necessarios
     //para se tornar um ticker
-
-    _heightAnimation = Tween(
-            begin: const Size(double.infinity, 310),
-            end: const Size(double.infinity, 400))
-        .animate(CurvedAnimation(
+    // muda a opacidade conforme a animacao de 0 a 1
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
       parent: _controller!,
       curve: Curves.linear,
     ));
-    //chamando os et pra alterar o estado do componente
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1.5),
+      end: const Offset(0, 0),
+    ).animate(CurvedAnimation(
+      parent: _controller!,
+      curve: Curves.linear,
+    ));
   }
 
   @override
@@ -183,22 +188,35 @@ class _AuthFormState extends State<AuthForm>
                   return null;
                 },
               ),
-              if (_isSignup())
-                TextFormField(
-                  decoration:
-                      const InputDecoration(labelText: 'Confirmar Senha'),
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: true,
-                  validator: _isLogin()
-                      ? null
-                      : (_password) {
-                          final password = _password ?? '';
-                          if (password != _passwordController.text) {
-                            return 'Senhas informadas não conferem.';
-                          }
-                          return null;
-                        },
+              // if (_isSignup())
+              AnimatedContainer(
+                constraints: BoxConstraints(
+                  minHeight: _isLogin() ? 0 : 60,
+                  maxHeight: _isLogin() ? 0 : 120,
                 ),
+                duration: const Duration(milliseconds: 300),
+                child: FadeTransition(
+                  opacity: _opacityAnimation!,
+                  child: SlideTransition(
+                    position: _slideAnimation!,
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Confirmar Senha'),
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: true,
+                      validator: _isLogin()
+                          ? null
+                          : (_password) {
+                              final password = _password ?? '';
+                              if (password != _passwordController.text) {
+                                return 'Senhas informadas não conferem.';
+                              }
+                              return null;
+                            },
+                    ),
+                  ),
+                ),
+              ),
               const SizedBox(height: 20),
               if (_isLoading)
                 const CircularProgressIndicator()
